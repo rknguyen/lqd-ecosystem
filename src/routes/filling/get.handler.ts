e1109125382d9bdc1fd071391a8e9c0ba9859ff9
rknguyen <nmhuy12@gmail.com>
2019-12-11 00:58:07 +0700

    fix: add null

--- src/routes/filling/get.handler.ts
+++ src/routes/filling/get.handler.ts
@@ -1,27 +1,40 @@
-import isObjectID from '../../utils/objectid'
-import { RKSuccess } from '../../utils/response'
+import isObjectID from "../../utils/objectid";
+import { RKSuccess } from "../../utils/response";
 
-import Flilling from '../../models/Filling'
-import Admin from '../../models/Admin'
+import Flilling from "../../models/Filling";
+import Admin from "../../models/Admin";
+import Information from "../../models/Information";
+import Document from "../../models/Document";
 
 async function Handler(req: any, res: any) {
-  const { fillingID } = req.all
-  let fillingCond: any = isObjectID.test(fillingID) ? { _id: fillingID } : {}
+  const { fillingID } = req.all;
+  let fillingCond: any = isObjectID.test(fillingID) ? { _id: fillingID } : {};
 
-  const adminCond = { userID: req.user._id.toString() }
-  const admin = await Admin.findOne(adminCond)
+  const adminCond = { userID: req.user._id.toString() };
+  const admin = await Admin.findOne(adminCond);
 
   if (admin === null) {
-    fillingCond = { 
-      ...fillingCond, 
-      ownerID: req.user._id.toString() 
-    }
+    fillingCond = {
+      ...fillingCond,
+      ownerID: req.user._id.toString()
+    };
   }
 
-  const filling: any = await Flilling.find(fillingCond)
+  const filling: any = await Flilling.find(fillingCond);
+
+  let fillingList = [];
+  for (let fill of filling) {
+    fill = fill.toObject();
+    const { ownerID, documentID } = fill;
+    const user: any = await Information.findOne({ userId: ownerID });
+    const docs: any = await Document.findOne({ _id: documentID });
+    fill.ownerName = user.name;
+    fill.docName = docs.name;
+    fillingList.push(fill);
+  }
   return res.json(
-    (new RKSuccess('Lấy bản ghi thành công', filling)).toJSON()
-  )
+    new RKSuccess("Lấy bản ghi thành công", fillingList).toJSON()
+  );
 }
 
-export default Handler
+export default Handler;
